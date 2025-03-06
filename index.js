0cfa11fb377b8d4bef461c359e3c32a3582b0574
Joykinuthia <joyrosekinuthia@gmail.com>
2025-03-06 17:42:58 +0300

    first class functions continued

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+// Code your solution in this file
+
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2)
+}
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(2,4)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+    console.log(selectingDrivers);
+
+function createFareMultiplier(fareMultiplier) {
+    return function(fare) {
+        return fare * fareMultiplier
+    };
+}
+
+    const fareDoubler = createFareMultiplier(2);
+    const fareTripler = createFareMultiplier(3);
+
+    console.log(fareDoubler)
+    console.log(fareDoubler)
+
+
+function selectDifferentDrivers(arrayOfDrivers , drivers) {
+    return drivers(arrayOfDrivers);
+}
